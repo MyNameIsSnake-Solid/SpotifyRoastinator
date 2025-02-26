6f93205ccb64c15f8234dfbd925e3715dc45d65c
Tryorse <mk04106770@gmail.com>
2025-02-26 01:14:15 +0000

    "animated" loading screen

--- RobertPattinson.js
+++ RobertPattinson.js
@@ -4,7 +4,7 @@ function clearPage()
 {
     const elements = document.querySelectorAll("*");
 
-    console.log(elements.length);
+    // console.log(elements.length);
     
     for (x = 0; x < elements.length; x++)
     {
@@ -18,4 +18,26 @@ function removeElement(element_to_delete)
     element_to_delete.remove();
 }
 
+function wait(seconds) {
+    return new Promise(resolve => setTimeout(resolve, seconds * 1000));
+  }
+
 // loading screen
+async function loopAction(paragraphElement)
+{
+    // console.log(paragraphElement + " " + paragraphElement.innerHTML);
+    count = 0;
+    while (true){
+        paragraphElement.innerHTML = "Loading";
+
+        for (x = 0; x < count; x++){
+            paragraphElement.innerHTML += ".";
+        }
+        // console.log(count);
+
+        await wait(1);
+        count++;
+        if (count >= 4)
+            count = 0;
+    }
+}
