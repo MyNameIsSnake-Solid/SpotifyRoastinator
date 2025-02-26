bab78ff8f514a8a9514eced69cf222546df335fa
MyNameIsSoliidSnake <PhilipValdehuesa12242@gmail.com>
2025-02-25 19:07:37 -0800

    Added stuff to the  Spotify API

--- spotify/spotify.py
+++ spotify/spotify.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, session, url_for, redirect, request
 
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth
@@ -13,6 +13,45 @@ client_id = 'e3ebd892691a4853bc0a34aac11e852a'
 client_secret = 'a007d43223ac4b27b6d40fb4f05a9fb4'
 redirect_uri = 'http://localhost:5000/callback'
 scope = 'playlist-read-private'
+
+cache_handler = FlaskSessionCacheHandler(session)
+sp_oauth = SpotifyOAuth(
+    client_id = client_id,
+    client_secret = client_secret,
+    redirect_uri = redirect_uri,
+    scope = scope,
+    cache_handler = cache_handler,
+    show_dialog = True
+)
+sp = Spotify(auth_manager=sp_oauth)
+
+app.route('/')
+def home(): 
+    if not sp_oauth.validate_token(cache_handler.get_cached_token()): 
+        auth_url = sp_oauth.get_authorize_url()
+        return redirect(auth_url)
+    return redirect(url_for('get_playlists'))
+
+app.route('/callback')
+def callback():
+    sp_oauth.get_access_token(request.args['code'])
+
+app.route('/get_playlists')
+def get_playlists():
+    if not sp_oauth.validate_token(cache_handler.get_cached_token()): 
+        auth_url = sp_oauth.get_authorize_url()
+        return redirect(auth_url)
+    
+    playlists = sp.current_user_playlists()
+    playlists_info = [(pl['name'], pl['external_urls']['spotify']) for pl in playlists['items']]
+    playlists_html = '<br>'.join([f'{name}: {url}' for name, url in playlists_info])
+
+    return playlists_html
+
+app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
