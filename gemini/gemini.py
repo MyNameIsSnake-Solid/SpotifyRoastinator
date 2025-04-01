6662fcebce686b1ae4dde71c0129e47f8b1c7bb3
Philip Valdehuesa <PhilipValdehuesa12242@gmail.com>
2025-04-01 22:46:44 +0000

    Did some stuff

---
+++ gemini/gemini.py
@@ -0,0 +1,53 @@
+import base64
+import os
+from google import genai
+from google.genai import types
+
+
+def generate():
+    client = genai.Client(
+        api_key=os.environ.get("GEMINI_API_KEY"),
+    )
+
+    model = "gemini-2.0-flash"
+    contents = [
+        types.Content(
+            role="user",
+            parts=[
+                types.Part.from_text(text="""Hello!"""),
+            ],
+        ),
+        types.Content(
+            role="model",
+            parts=[
+                types.Part.from_text(text="""Alright, let's do this. Lay it on me. Tell me your top artists and songs. Don't be shy, I'm ready to dissect your musical soul...and probably find some skeletons in its closet. Don't worry, I'll be gentle...ish. 😉
+"""),
+            ],
+        ),
+        types.Content(
+            role="user",
+            parts=[
+                types.Part.from_text(text="""INSERT_INPUT_HERE"""),
+            ],
+        ),
+    ]
+    generate_content_config = types.GenerateContentConfig(
+        temperature=2,
+        top_p=0.95,
+        top_k=40,
+        max_output_tokens=8192,
+        response_mime_type="text/plain",
+        system_instruction=[
+            types.Part.from_text(text="""You are a music analyst (you analyze songs and artists). Your task is to analyze them and generate a roast all based on their top songs and artists.  When you roast them, make sure to be playful and have fun with it (but of course ackowledge about the problem with their music taste)! Remember, you have all the knowledge in music so feel free to be witty, playful, and biting as much as possible!"""),
+        ],
+    )
+
+    for chunk in client.models.generate_content_stream(
+        model=model,
+        contents=contents,
+        config=generate_content_config,
+    ):
+        print(chunk.text, end="")
+
+if __name__ == "__main__":
+    generate()
--- spotify/spotify.py
+++ spotify/spotify.py
@@ -1,58 +1,37 @@
+from flask import Flask, render_template, redirect, request, session, url_for
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from dotenv import load_dotenv
 import os
 
-from flask import Flask, session, url_for, redirect, request
+load_dotenv()
 
-from spotipy import Spotify
-from spotipy.oauth2 import SpotifyOAuth
-from spotipy.cache_handler import FlaskSessionCacheHandler
 
-app = Flask("__name__")
-app.config['SECRET_KEY'] = os.urandom(64)
 
-client_id = 'e3ebd892691a4853bc0a34aac11e852a'
-client_secret = 'a007d43223ac4b27b6d40fb4f05a9fb4'
-redirect_uri = 'http://localhost:5000/callback'
-scope = 'playlist-read-private'
+CLIENT_ID = os.environ.get('CLIENT_KEY')
+CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
+REDIRECT_URI = 'http://localhost:5000'
 
-cache_handler = FlaskSessionCacheHandler(session)
-sp_oauth = SpotifyOAuth(
-    client_id = client_id,
-    client_secret = client_secret,
-    redirect_uri = redirect_uri,
-    scope = scope,
-    cache_handler = cache_handler,
-    show_dialog = True
+
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        redirect_uri=REDIRECT_URI,
+        scope='user-top-read'
+    )
 )
-sp = Spotify(auth_manager=sp_oauth)
 
-app.route('/')
-def home(): 
-    if not sp_oauth.validate_token(cache_handler.get_cached_token()): 
-        auth_url = sp_oauth.get_authorize_url()
-        return redirect(auth_url)
-    return redirect(url_for('get_playlists'))
 
-app.route('/callback')
-def callback():
-    sp_oauth.get_access_token(request.args['code'])
 
-app.route('/get_playlists')
-def get_playlists():
-    if not sp_oauth.validate_token(cache_handler.get_cached_token()): 
-        auth_url = sp_oauth.get_authorize_url()
-        return redirect(auth_url)
-    
-    playlists = sp.current_user_playlists()
-    playlists_info = [(pl['name'], pl['external_urls']['spotify']) for pl in playlists['items']]
-    playlists_html = '<br>'.join([f'{name}: {url}' for name, url in playlists_info])
+top_artists = sp.current_user_top_artists(limit=5, time_range='long_term')
+top_songs = sp.current_user_top_tracks(limit=5, time_range='long_term')
 
-    return playlists_html
+artists = [f"{idx}. {artist['name']}" for idx, artist in enumerate(top_artists['items'], 1)]
+songs = [f"{idx}. {song['name']}" for idx, song in enumerate(top_songs['items'], 1)]
 
-app.route('/logout')
-def logout():
-    session.clear()
-    return redirect(url_for('home'))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#
 
+print(artists)
+print(songs)
