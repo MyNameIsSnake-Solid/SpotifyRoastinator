6133ede6c6361908df27896391b19077a84803b7
Tryorse <mk04106770@gmail.com>
2025-04-01 16:11:19 -0700

    .

--- spotify/spotify.py
+++ spotify/spotify.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request, session, url_for
+from flask import Flask, redirect, request, session, url_for
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
@@ -27,6 +27,7 @@ sp = spotipy.Spotify(
 top_artists = sp.current_user_top_artists(limit=5, time_range='long_term')
 top_songs = sp.current_user_top_tracks(limit=5, time_range='long_term')
 
+# for loops to get tge top 5 artists and songs
 artists = [f"{idx}. {artist['name']}" for idx, artist in enumerate(top_artists['items'], 1)]
 songs = [f"{idx}. {song['name']}" for idx, song in enumerate(top_songs['items'], 1)]
 
